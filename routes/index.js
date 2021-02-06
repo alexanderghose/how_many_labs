b4a7533950ec70e43c4977c4f104dc58b9f4977e
alex ghose <alex.ghose@generalassemb.ly>
2021-02-06 11:51:22 -0500

    remove ref to old creds file

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 let { GoogleSpreadsheet } = require('google-spreadsheet');
-let creds = require('../client_secret.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
