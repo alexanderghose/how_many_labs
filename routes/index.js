9d2a02fabae513f7f9e97be6c123d0f15cd75b21
alex ghose <alex.ghose@generalassemb.ly>
2021-02-06 10:46:21 -0500

    fix styling and make it use the real sheet

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+let { GoogleSpreadsheet } = require('google-spreadsheet');
+let creds = require('../client_secret.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/36", async function(req, res){
+  
+  // Identifying which document we'll be accessing/reading from
+  // if (!process.env.SEI_36_DELIVERABLES_SHEET) {
+  //   return res.send("error")
+  // }
+  try {
+    var doc = new GoogleSpreadsheet(process.env.SEI_36_DELIVERABLES_SHEET);  
+    await doc.useServiceAccountAuth(creds) // Authentication
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[7]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+    let first_valid_row = 4;
+    let last_valid_row = 14;
+    let labs = [];
+    await sheet.loadCells(`A${first_valid_row}:D${last_valid_row}`);
+    // getCell is zero-indexed. so for row A1 it's getCell(0,0).
+    for (let row = first_valid_row-1; row <= last_valid_row-1; row++) {
+      obj = {
+        codename:sheet.getCell(row,2).value,
+        completedLabs:sheet.getCell(row,3).value,
+      };
+      labs.push(obj);
+    }
+    res.render('labs.ejs', {labs});
+
+  } catch(error) {
+    res.status(500).send({error: error})
+  }
 });
 
 module.exports = router;
